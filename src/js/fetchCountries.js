411965b5007ff64e4310229b670e0ffd44c5ff08
olegscherbak <nix.001@ukr.net>
2021-10-19 19:11:11 +0300

    hw-js-12-countries-v2

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,8 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
+const BASE_URL = 'https://restcountries.com/v2/name';
 
 function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => {
+  return fetch(`${BASE_URL}/${searchQuery}`).then(response => {
     return response.json();
   });
 }
-
 export default { fetchCountries };
